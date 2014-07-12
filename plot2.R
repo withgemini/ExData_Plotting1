e8fe544197e9eb28af61fc623c3313e736650af9
withgemini <withgemini@users.noreply.github.com>
2014-07-12 09:11:13 -0700

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,9 @@
+# Reconstructs plot 2 of the assignment and saves it in plot2.png, using the base plotting system
+
+# Code for reading the data so that the plot can be fully reproduced 
+
+
+
+# Code that creates the PNG file with a width of 480 pixels and a height of 480 pixels
+
+ 
