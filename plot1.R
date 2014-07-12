7987463b45dfa047c2285151744c83c2e114467f
withgemini <withgemini@users.noreply.github.com>
2014-07-12 16:22:44 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -4,12 +4,10 @@
 # Code for reading the data so that the plot can be fully reproduced 
 rawData<-read.table('household_power_consumption.txt', header=T, sep=";", nrows=70000, na.strings="?")
 
-#Create a new variable that has both date and time
 dateAndTime <-paste(rawData$Date, rawData$Time)
 rawData$datetime<-strptime(dateAndTime, format="%d/%m/%Y %H:%M:%S")
 
 rawData$Date<-as.Date(rawData$Date, format="%d/%m/%Y")
-
 
 data1<-subset(rawData, Date=='2007-2-1')
 data2<-subset(rawData, Date=='2007-2-2')
@@ -18,5 +16,8 @@ cleanData<-rbind(data1,data2)
 
 
 # Code that creates the PNG file with a width of 480 pixels and a height of 480 pixels
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+with(cleanData, hist(Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power"))
+dev.off()
 
  
